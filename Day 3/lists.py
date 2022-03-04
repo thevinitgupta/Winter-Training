ceed23a57e7a91a5ada38ee85f326963b88a357c
Vinit Gupta <vinitman35@gmail.com>
2022-03-04 16:52:45 +0530

    editing of different types of lists

--- Day 3/lists.py
+++ Day 3/lists.py
@@ -31,3 +31,22 @@ print(l1[-1][-1])
 l1 = [[[1,2],[3,4]],[[5,6],[7,8]]] #* homo genous 3D list
 print(l1[1][1][0])
 print(l1[-2][-1][-2])
+
+
+l1=[1,2,3,4]
+l1[1:2] = [100,200,300]
+print(l1)
+l1.append(600) # add 1 element
+l1.extend([101,202,303]) #append multiple values
+print(l1)
+
+l1.append([10001,10101])  #!always adds only 1 value in the end
+print(l1)
+
+l1.extend('goa') #!append 'g', 'o' and 'a' separately -> always adds mutiple values
+print(l1)
+
+l1.insert(1,150)
+print(l1)
+
+
