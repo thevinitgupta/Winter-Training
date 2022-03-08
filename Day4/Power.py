714b672d75d1cf1444691217725d155b8bcfaee7
Vinit Gupta <vinitman35@gmail.com>
2022-03-08 18:32:05 +0530

    arbitary list of arguments

---
+++ Day4/Power.py
@@ -0,0 +1,21 @@
+
+# default values for arguments
+def power(a=1,b=1):
+    return a**b
+
+print(power(2,3))
+print(power(2))
+print(power())
+
+
+print(power(b=2,a=3))
+
+
+def sum(*num):
+    res = 0
+    for i in num:
+        res += i
+    return res
+
+print(sum(1,2))
+print(sum(1,2,3,4,5,6,7))
