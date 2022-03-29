c69a6eaf7a74682aca531b2fa03721651145c38a
Vinit Gupta <vinitman35@gmail.com>
2022-03-29 09:22:50 +0530

    make append method for my array

--- DynamicArrays/DynamicArray.py
+++ DynamicArrays/DynamicArray.py
@@ -8,4 +8,36 @@ class MyList:
 
     def __make_array(self, capacity):
         #*using the ctype module to make an array of C type with size capacity and returns it
-        return (capacity*ctypes.py_object)
+        return (capacity*ctypes.py_object)()
+
+    def __len__(self):
+        return self.n
+
+    def append(self,item):
+        if self.n==self.size:
+            #array is full
+            self.__resize(self.size*2)
+
+        #append
+        self.A[self.n] = item
+        self.n = self.n+1
+
+    def __resize(self,new_capacity):
+        #create a new array(double the size)
+        B = self.__make_array(new_capacity)
+        #copy B to A
+        for i in range(self.n):
+            B[i] = self.A[i]
+
+        #make B -> A
+        self.A = B
+        self.size = new_capacity
+
+l = MyList()
+print(len(l))
+l.append(4)
+l.append(1)
+l.append(7)
+l.append(9)
+l.append(0)
+print(len(l))
